848af58719cb431f6d989a960d26db7b543fda86
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-02-16 18:24:31 +1100

    attempt to extract arxiv text for input list

--- train_topics.py
+++ train_topics.py
@@ -3,9 +3,9 @@ print("[train_topics] using MultinominalNB for training labelled topics")
 
 #grab documents for training
 import pandas as pd
+import extract_arxiv
 reports = pd.read_csv("source data/papers.csv", dtype=str)
-# printing all columns of the dataframe 
-print(reports)
+reports['paper_info'] = reports['url'].apply(extract_arxiv.extract_paper_info)
 
 # filtering for training data on basis of both filters
 training_reports = reports[reports['trainFlag'] == "y"]
@@ -14,12 +14,7 @@ print(training_reports)
 # Make labels
 # Assuming you have already filtered the DataFrame as 'training_reports'
 # You can create the labeled_data list like this:
-labeled_data = list(zip(training_reports['label'], training_reports['URL']))
-
-# Print the resulting list
-print("Labeled data:")
-for label, url in labeled_data:
-    print(f"({label}, {url})")
+labeled_data = list(zip(training_reports['label'], training_reports['paper_info']))
 
 import MultinomialNB
 print("training")
