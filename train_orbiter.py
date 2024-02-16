361de475cc750d138bd093ccc1465f14f3f21fe4
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-17 02:20:49 +1100

    targetPerc renamed trainPerc return clf and vectoriser

--- train_orbiter.py
+++ train_orbiter.py
@@ -5,13 +5,13 @@ import model_funcs.MultinomialNB as MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-def train(source_data_filename, targetPerc=.2):
+def train(source_data_filename, testPerc=.2):
     #grab documents for training
     reports = pd.read_csv(source_data_filename, dtype=str)
     reports['paper_info'] = reports['URL'].apply(extract_arxiv.extract_paper_info)
 
     # Split the data into training and testing sets
-    train_reports, test_reports = train_test_split(reports, test_size=targetPerc, random_state=42)
+    train_reports, test_reports = train_test_split(reports, test_size=testPerc, random_state=42)
 
     # Make labels for training data
     labeled_data = list(zip(train_reports['label'], train_reports['paper_info']))
@@ -27,3 +27,5 @@ def train(source_data_filename, targetPerc=.2):
     accuracy = accuracy_score(test_reports['label'], test_predictions)
     print(f"Training Accuracy: {accuracy * 100:.2f}%")
 
+    #return
+    return(clf, vectoriser)
