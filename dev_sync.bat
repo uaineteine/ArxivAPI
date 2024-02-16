273bce3a9b60a5ec6806e2c0d7992e5758926545
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-16 21:39:34 +1100

    make dev_sync batch

---
+++ dev_sync.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Checkout the master branch
+git checkout master
+
+REM Pull all changes from origin
+git pull origin master
+
+REM Checkout the dev-main branch
+git checkout dev-main
+
+REM Merge master into dev-main
+git merge master
+
+REM Push changes back to the remote
+git push origin dev-main
