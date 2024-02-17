5012d3f1c1ab0b54ab6f3bb78265ec5df26752d6
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-17 13:02:24 +1100

    run training method

---
+++ run_training.py
@@ -0,0 +1,21 @@
+print("run_training] import libraries")
+import os
+import glob
+import train_orbiter
+
+def list_csv_files(directory):
+    return glob.glob(os.path.join(directory, '*.csv'))
+
+def select_dataset():
+    csv_files = list_csv_files('source data')
+    for i, filename in enumerate(csv_files):
+        print(f"{i+1}. {filename}")
+    selected = int(input("Enter the number of the dataset you want to train on: ")) - 1
+    return csv_files[selected]
+
+def main():
+    selected_dataset = select_dataset()
+    print(f"You selected {selected_dataset}")
+    clf, vectoriser = train_orbiter.train(selected_dataset)
+
+main()
--- test_train_orbiter.py
+++ test_train_orbiter.py
@@ -2,9 +2,3 @@ print("[test_train_orbiter] import libraries")
 import train_orbiter
 
 train_orbiter.train("source data/test_data.csv")
-
-# Save the model
-#dump(clf, "trained_models/processing/filename.joblib") 
-
-# Load the model
-#clf = load('filename.joblib') 
