1512377c9ebfa6dec63f9f285fcbfaea87f2d350
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-18 14:01:52 +1100

    api status

---
+++ arxiv_api/get_status.py
@@ -0,0 +1,33 @@
+print("[get_status] importing libraries")
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def scrape_info(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Scrape information from div with class 'container container-md'
+    container = soup.find('div', {'class': 'container container-md'})
+
+    # Find the current status section and extract the status message
+    current_status_section = container.find('section', {'id': 'current-status'})
+    status_message = current_status_section.find('h2').text
+
+    # Use regex to find the status message
+    match = re.search(r'All systems are go', status_message)
+
+    if match:
+        print("[get_status] Main Status: All systems are go")
+    else:
+        print("[get_status] Main Status: Not found")
+
+    # Find every child from ul with class 'list-components' 
+    # that is an li tag with class 'list-component has-nested-components'
+    list_items = soup.find('ul', {'class': 'list-components'}).find_all('li', {'class': 'list-component has-nested-components'})
+    
+    #for item in list_items:
+        #print(item)
+
+# Call the function with your URL
+scrape_info('https://status.arxiv.org/')
