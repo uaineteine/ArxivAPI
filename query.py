145f2b11ed5b7feaa0ab0191f648958604d13305
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-02-22 01:22:19 +1100

    make query func

--- query.py
+++ query.py
@@ -1,5 +1,7 @@
 https://export.arxiv.org/api/query?search_query=all:the&id_list=1802.06593,0704.0003&start=0&max_results=100
 
+import urllib.request as libreq
+
 pgSize = 100
 
 def page_query_url(base_url, startIndex):
@@ -9,3 +11,7 @@ def build_base_query_url(idlist):
   comma_ids = ', '.join(idlist) #make into a comma separated list
   return f"https://export.arxiv.org/api/query?&id_list={comma_ids}"
 
+def extract_query(url_in):
+    with libreq.urlopen(url_in) as url:
+      r = url.read()
+    return(r)
