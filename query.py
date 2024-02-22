90ce8e4526a495e5e4740a29ebd98b5782c1fb26
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-22 22:16:18 +1100

    parse xml to get all children of entry

--- query.py
+++ query.py
@@ -3,6 +3,7 @@
 
 print("[ArxivAPI::query] importing libraries")
 import requests
+import pandas as pd
 
 def page_query_url(base_query, startIndex, pgSize = 100):
   return f"{base_query}&start={startIndex}&max_results={pgSize}"
@@ -17,3 +18,20 @@ def extend_query_with_search(base_query, search_terms):
 def xml_query(url_in):
   r = requests.get(url_in)
   return r.text
+
+import xml.etree.ElementTree as ET
+
+def parse_xml(xml_text):
+    # Parse the XML text
+    root = ET.fromstring(xml_text)
+
+    # Define the namespace
+    ns = {'ns': 'http://www.w3.org/2005/Atom'}
+
+    # Find all 'entry' tags
+    entries = root.findall('.//ns:entry', ns)
+
+    # For each 'entry', get all its children
+    entry_xmls = [ET.tostring(entry, encoding='unicode') for entry in entries]
+
+    return entry_xmls
