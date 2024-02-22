90970e5e386e0208458377dfa22d5ec62ad8a978
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-02-22 21:42:32 +1100

    xml query

--- query.py
+++ query.py
@@ -10,10 +10,10 @@ def page_query_url(base_url, startIndex):
   return f"{base_url}&start={startIndex}&max_results={pgSize}"
 
 def build_base_query_url(idlist):
-  comma_ids = ', '.join(idlist) #make into a comma separated list
+  comma_ids = ",".join(idlist) #make into a comma separated list
   return f"https://export.arxiv.org/api/query?&id_list={comma_ids}"
 
-def extract_query(url_in):
+def xml_query(url_in):
     with libreq.urlopen(url_in) as url:
       r = url.read()
     return(r)
--- test_extract.py
+++ test_extract.py
@@ -1,5 +1,15 @@
+print("[test_extract] testing extract using soup")
 import arxiv_paper
 
 p = arxiv_paper.Paper("0704.0003")
 p.extract_using_soup()
 print(p)
+
+
+
+print("[test_extract] testing extract using API")
+import query
+q = query.build_base_query_url(["1802.06593", "0704.0003"])
+q = query.page_query_url(q, 0)
+xml = query.xml_query(q)
+print(xml)
