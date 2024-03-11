e4ab6faeb08aeaf3fb9d9610dc2c668f35f1c6c3
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-03-11 11:44:42 +1100

    improvement to directory tree ignoring the git dir

--- automation/devscripts/get_directory_tree.py
+++ automation/devscripts/get_directory_tree.py
@@ -1,7 +1,19 @@
 import os
 
 def list_files(startpath):
+    # Read .gitignore and create a list of ignored paths
+    with open('.gitignore', 'r') as f:
+        ignored = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+    # Add .git and .vscode to the list of ignored paths
+    ignored.append('.git')
+    ignored.append('.vscode')
+
     for root, dirs, files in os.walk(startpath):
+        # Skip directories and files that are in .gitignore
+        dirs[:] = [d for d in dirs if os.path.relpath(os.path.join(root, d), startpath) not in ignored]
+        files = [f for f in files if os.path.relpath(os.path.join(root, f), startpath) not in ignored]
+
         level = root.replace(startpath, '').count(os.sep)
         indent = ' ' * 4 * (level)
         print('{}{}/'.format(indent, os.path.basename(root)))
